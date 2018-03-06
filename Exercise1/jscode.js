b450bd70b32c61877c96fc206151c0a024b4c067
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-06 16:36:48 +0800

    modified the alert message

--- Exercise1/jscode.js
+++ Exercise1/jscode.js
@@ -1,7 +1,7 @@
 function checkValidation() {
   var password=document.getElementById('password').value;
   if (password != "12345") {
-    alert("Caution! Password incorrect");
+    alert("Caution!\nPassword incorrect");
   }else{
     var y=document.getElementById('sendItem');
     y.className += "leftTransition";
