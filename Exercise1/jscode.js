23f4df79bef17ff0f4e3f07b4c96427d342d400d
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-06 15:55:22 +0800

    jscode.js: alert function added for incorrect password.

---
+++ Exercise1/jscode.js
@@ -0,0 +1,8 @@
+function checkValidation() {
+  var password=document.getElementById('password').value;
+  if (password != "12345") {
+    alert("Caution! Password incorrect");
+  }else{
+    //something
+  }
+}
