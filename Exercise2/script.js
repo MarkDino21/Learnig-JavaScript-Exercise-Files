f49d036bb91b99802405da7bf8865a66c99560a4
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-08 16:49:30 +0800

    script.js: css codes in console.log

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -1,2 +1,6 @@
 console.log('Hello world');
 console.error('Error code');
+console.warn('Warning Message');
+
+console.log('%cHello world','color:green');
+console.log('%cHello world','background-color:green; color:red; font-size: 15px; font-weight: bold');
