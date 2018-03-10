552d1ccc2dd2948cdb3a1e9bc8607e70e91c3780
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-10 10:41:35 +0800

    fix bug in array task. change to string

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -5,7 +5,7 @@ console.warn('Warning Message');
 console.log('%cHello world','color:green');
 console.log('%cHello world','background-color:green; color:red; font-size: 15px; font-weight: bold');
 */
-var task = [cooking, laundry, wasshing dishes];
+var task = ["cooking", "laundry", "wasshing dishes"];
 var progress = ["done", "in progress", "to do"];
 
 var question1 = prompt("Which task from 1-4");
