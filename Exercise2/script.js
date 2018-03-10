994adb9c4301eef00ebaec6b361542917fbdcdd3
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-10 10:59:00 +0800

    try length property of array(task)

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -1,4 +1,5 @@
-/*console.log('Hello world');
+/*-----------Exercise 1.1------------------
+console.log('Hello world');
 console.error('Error code');
 console.warn('Warning Message');
 
@@ -7,15 +8,15 @@ console.log('%cHello world','background-color:green; color:red; font-size: 15px;
 */
 
 
-//Exercise 1.2
-var task = ["cooking", "laundry", "wasshing dishes"];
+//---------------Exercise 1.2--------------
+var task = ["cooking", "laundry", "wasshing dishes","sleeping"];
 var progress = ["done", "in progress", "to do"];
 
-var question1 = prompt("Which task from 1-4");
+var question1 = prompt("Which task from 1-" + task.length + "?");
 var question2 = prompt("what status?: \n1=done, 2=in progress, 3=to do");
 var task1=Number(question1)-1;
 var progress1=Number(question2)-1;
 //question1;
 //question2;
 
-console.log('today you will ' + task[task1] + 'status of work is ' + progress[progress1]);
+console.log('today you will ' + task[task1] + ' status of work is ' + progress[progress1]);
