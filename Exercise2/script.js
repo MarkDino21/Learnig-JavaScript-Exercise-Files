b101e0af7d9a9d125ea362781b88e58a04886ac4
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-10 10:40:34 +0800

    exercise 1.2

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -1,6 +1,18 @@
-console.log('Hello world');
+/*console.log('Hello world');
 console.error('Error code');
 console.warn('Warning Message');
 
 console.log('%cHello world','color:green');
 console.log('%cHello world','background-color:green; color:red; font-size: 15px; font-weight: bold');
+*/
+var task = [cooking, laundry, wasshing dishes];
+var progress = ["done", "in progress", "to do"];
+
+var question1 = prompt("Which task from 1-4");
+var question2 = prompt("what status?: \n1=done, 2=in progress, 3=to do");
+var task1=Number(question1)-1;
+var progress1=Number(question2)-1;
+//question1;
+//question2;
+
+console.log('today you will ' + task[task1] + 'status of work is ' + progress[progress1]);
