3fd8f46f23e6a870e83fbd541190828ef2159e5a
MarkDino21 <markdinopelonia447@gmail.com>
2018-03-13 10:05:25 +0800

    Challenge specified

--- Exercise3/script.js
+++ Exercise3/script.js
@@ -1,1 +1,6 @@
 //-----------Exercise 3 Array------------------
+var newHolder = ['cat','dog','rabbit', 'bird'];
+console.log(newHolder);
+//**Challenge: target output
+//['Horse','fish','fish','fish','fish','fish','fish','fish','rabbit',
+//'dog','sheep']
