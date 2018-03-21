6c2b7b6acf02831a81a49f88bf20d040daf0b800
MarkDino1 <markdinopelonia447@gmail.com>
2018-03-21 21:52:56 +0800

    test 2 update succeded.

--- Exercise5/script.js
+++ Exercise5/script.js
@@ -13,7 +13,14 @@ var friends={
   }
 };
 var myFriends =[bob,{age:30, hair: "black"}];
-//test: retrieve.
+//test 1: retrieve.
 
 console.log(friends.friend1.age);
+console.log(myFriends[0].age);
+
+//test 2: update.
+friends.friend1.age=25;
+console.log(friends.friend1.age);
+
+myFriends[0].age=15;
 console.log(myFriends[0].age);
