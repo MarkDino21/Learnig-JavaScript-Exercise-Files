c73b0627a94242cc006acabb432601bc2ca1c85d
MarkDino1 <markdinopelonia447@gmail.com>
2018-03-27 16:55:12 +0800

    test1 succeded

--- Exercise6/script.js
+++ Exercise6/script.js
@@ -1,1 +1,6 @@
+var output = document.getElementById('title1');
+var val = document.getElementById('val');
 
+//test1
+output.innerHTML= "<h1>" + document.title + "</h1>";
+val.value = "Hello, World";
