e08465bbc58b431fb74f2a324b1b7cfa28b17ebc
MarkDino1 <markdinopelonia447@gmail.com>
2018-03-27 17:41:19 +0800

    test 2 succeded

--- Exercise6/script.js
+++ Exercise6/script.js
@@ -4,3 +4,7 @@ var val = document.getElementById('val');
 //test1
 output.innerHTML= "<h1>" + document.title + "</h1>";
 val.value = "Hello, World";
+
+//test2
+output.innerText = "<h1>New Title</h1>";
+val.value = "New text";
