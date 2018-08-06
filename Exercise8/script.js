e73b8c7360ea22db32f528693579370f51805e40
MarkDino21 <markdinopelonia447@gmail.com>
2018-08-06 22:04:27 +0800

    undo the convert of x and add condition to the loop

--- Exercise8/script.js
+++ Exercise8/script.js
@@ -1,10 +1,15 @@
 //--test1 succedded-----
 var output = document.getElementById('output');
 var x= prompt("Enter count of loop");
-x= Number(x);
+//x= Number(x);
 var i =0;
-do {
-  i++;
-  output.innerHTML += "count " + i + "<br>";
+if(typeof(x)=='number'){
+  do {
+    i++;
+    output.innerHTML += "count " + i + "<br>";
+  }
+  while (i<x);
 }
-while (i<x);
+else{
+  alert("Please Enter a number only.");
+}
