5b957aecca7bb5d88987d7ad694cc49ccb4b94b8
MarkDino21 <markdinopelonia447@gmail.com>
2018-08-10 22:15:23 +0800

    restrain Test1 to Test3 code inside the function

--- Exercise8/script.js
+++ Exercise8/script.js
@@ -1,25 +1,27 @@
 //--test1 succedded-----
 //--test2 Succedded-----
 //--test3 Succedded-----
-var output = document.getElementById('output');
-var x= prompt("Enter a number of counts to loop");
+function test1ToTest3(){
+  var output = document.getElementById('output');
+  var x= prompt("Enter a number of counts to loop");
 
-while (Number.isNaN(parseInt(x))) {
-//while (isNaN(x)) {
-  alert("Please Enter a valid and positive numbers only.");
-  x= prompt("Enter a number of counts to loop");
-}
+  while (Number.isNaN(parseInt(x))) {
+  //while (isNaN(x)) {
+    alert("Please Enter a valid and positive numbers only.");
+    x= prompt("Enter a number of counts to loop");
+  }
 
-if (x>=0){
-  //var i=0
-  //do {
-  //  output.innerHTML += "count " + i + "<br>";
-  //  i++;
-  //}
-  //while (i<x);
+  if (x>=0){
+    //var i=0
+    //do {
+    //  output.innerHTML += "count " + i + "<br>";
+    //  i++;
+    //}
+    //while (i<x);
 
-//--test3: convert DO LOOP to FOR LOOP---
-  for(var i=0;i<x;i++){
-    output.innerHTML += "count " + i + "<br>";
+  //--test3: convert DO LOOP to FOR LOOP---
+    for(var i=0;i<x;i++){
+      output.innerHTML += "count " + i + "<br>";
+    }
   }
 }
