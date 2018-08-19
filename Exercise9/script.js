c0ec75346476ea6cafccfb1c99147a48184f925e
MarkDino21 <markdinopelonia447@gmail.com>
2018-08-19 15:17:17 +0800

    function for Addition done.

--- Exercise9/script.js
+++ Exercise9/script.js
@@ -1,3 +1,17 @@
-function functionName() {
-  
+
+// test1: simply calculator
+var output = document.getElementById('output');
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+
+function test1Calc(num1, num2, op, total) {
+  output.innerHTML+= num1 + " " + op + " " + num2 + " = " + total + "<br>";
+}
+
+function myAdd() {
+  var a = Number(num1.value);
+  var b = Number(num2.value);
+  var op = '+'
+  var total = a + b;
+  test1Calc(a, b, op, total);
 }
